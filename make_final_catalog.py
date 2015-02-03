c3019a3b19f6b5d270abaacde90d5b6cd1f20ba3
micaelabagley <micaela.bagley@gmail.com>
2015-02-03 10:38:57 -0600

    added threshold as option, default is 0.75"

--- make_final_catalog.py
+++ make_final_catalog.py
@@ -1,9 +1,4 @@
 #! /usr/bin/env python
-#######################################################################
-## Micaela Bagley
-## September 2014
-##
-#######################################################################
 import argparse
 from glob import glob
 import os
@@ -49,29 +44,64 @@ def make_reg(RA, Dec, reg, radius, color, width=1, nsource=np.array([])):
 
 
 
-def fix_datasec(image):
+def fix_header(image, wispfield):
     '''Fix the DATASEC keyword in the image's header to extend
        out to the full size of the image. After IRAF's imcombine,
        the DATASEC keyword should have been updated to include 
-       the new size of the image'''
+       the new size of the image
+       
+       Add the total exposure time of the combined images to 
+       the header
+    '''
     im,hdr = pyfits.getdata(image, header=True)
     nx1 = hdr['NAXIS1']
     nx2 = hdr['NAXIS2']
     hdr['DATASEC'] = '[1:%i,1:%i]' % (nx1-1, nx2-1)
+
+    if 'TEXPTIME' not in hdr:
+        ims = hdr['IMCMB*']
+        Nim = len(ims)
+        texptime = 0.
+        # add up all the exposure times
+        for i in range(Nim):
+            if os.path.splitext(ims[i])[1] != '.fits':
+                ims[i] = ims[i] + '.fits'
+            thdr = pyfits.getheader(os.path.join(wispfield,ims[i]))
+            texptime += thdr['exptime']
+        hdr.insert(19, ('TEXPTIME', texptime,
+                               'Total EXPTIME of combined images'))
     pyfits.writeto(image, im, hdr, clobber=True)
     
 
-def total_exptime(image, wispfield):
-    '''Add total exptime of combined images to the header'''
-    im,hdr = pyfits.getdata(image, header=True)
+def image_limits(image, wispfield):
+    '''Find the '''
+    # read in combined image
+    hdr = pyfits.getheader(image)
+    hdr_wcs = pywcs.WCS(hdr)
+    # list of images that were combined
     ims = hdr['IMCMB*']
-    texptime = 0.
     Nim = len(ims)
+    xx = np.zeros((Nim,2), dtype=int)
+    yy = np.zeros((Nim,2), dtype=int)
     for i in range(Nim):
-        texptime += pyfits.getheader(os.path.join(wispfield,ims[i]))['exptime']
-    hdr.insert(18, ('TEXPTIME', texptime,
-                           'Total EXPTIME of combined images'))
-    pyfits.writeto(image, im, hdr, clobber=True)
+        if os.path.splitext(ims[i])[1] != '.fits':
+            ims[i] = ims[i] + '.fits'
+        hd = pyfits.getheader(os.path.join(wispfield,ims[i]))
+        nx1 = hd['NAXIS1']
+        nx2 = hd['NAXIS2']
+        hd_wcs = pywcs.WCS(hd)
+        # find WCS coordinates of input image corners
+        coo = hd_wcs.wcs_pix2sky([[1,1],[1,nx1-1],[nx2-1,1],[nx2-1,nx1-1]],1)
+        # find corresponding pixel coords in combined image
+        pix = hdr_wcs.wcs_sky2pix(coo,1)
+        xx[i,0] = pix[0][1]
+        xx[i,1] = pix[1][1]
+        yy[i,0] = pix[0][0]
+        yy[i,1] = pix[2][0]
+
+    xmin,xmax = np.max(xx[:,0]), np.min(xx[:,1])
+    ymin,ymax = np.max(yy[:,0]), np.min(yy[:,1])
+    return xmin,xmax,ymin,ymax
 
 
 def setup(wispfield):
@@ -110,6 +140,7 @@ def setup(wispfield):
         SEmode = 'dual'
     else:
         print "Don't know what to do with %i images" % len(images)
+
     print '\n--------------------------------------------'
     print 'Running %s in %s image mode'%([x for x in images],SEmode)
     print '--------------------------------------------\n'
@@ -125,27 +156,28 @@ def setup(wispfield):
     zp = cal['zp']
     maglim = cal['maglim']
     sigma = cal['sigma']
+
     # dict of info for each filter
     info = {}
     for image in images:
-        # get filter
         filt = get_filter(image)
-
-        # fix the DATASEC keyword in the headers
-        fix_datasec(image)
-
-        # add total exptime of all combined images to header
-        total_exptime(image, wispfield)
-
+        # fix the DATASEC keyword in the headers, 
+        # and add TEXPTIME if necessary
+        fix_header(image, wispfield)
+        # find limits of each image in pixel coordinates
+        xmin,xmax,ymin,ymax = image_limits(image, wispfield)    
         # fill in info for image
         cat = os.path.splitext(image)[0] + '_final_cat.fits'
         info[filt] = {'cat':cat, 'cterm':cterm[filts == filt], \
                       'zp':zp[filts == filt], 'maglim':maglim[filts == filt], \
-                      'sigma':sigma[filts == filt]}
+                      'sigma':sigma[filts == filt], \
+                      'xmin':xmin, 'xmax':xmax, 'ymin':ymin, 'ymax':ymax}
+
     if WISPimages:
         info['WISPim'] = WISPimages[0]
     else:
         info['WISPim'] = []
+
     return info
 
 
@@ -177,7 +209,7 @@ def make_cat(info, WISPfilters, threshold, wispfield):
     filts = [x for x in filts if x != 'WISPim']
 
     # region file of Palomar objects
-    reg = open(os.path.join(wispfield,'Palomar-final.reg'), 'w')
+    reg = open(os.path.join(wispfield,'Palomar.reg'), 'w')
     reg.write('global color=green dashlist=8 3 width=1 font="helvetica 10 '
             'normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 '
             'move=1 delete=1 include=1 source=1 \n')
@@ -185,20 +217,26 @@ def make_cat(info, WISPfilters, threshold, wispfield):
 
     # read in Palomar catalogs
     pal1 = read_cat(Palcats[0])
-    pal1RA = pal1['X_WORLD']
-    pal1Dec = pal1['Y_WORLD']
     pal1x = pal1['X_IMAGE']
     pal1y = pal1['Y_IMAGE']
-    ID = pal1['NUMBER']
+    # take only sources within full overlap
+    f = 'i' if 'i' in filts else 'g'
+    w = np.where((pal1x > info[f]['xmin']) & (pal1x < info[f]['xmax']) & 
+                 (pal1y > info[f]['ymin']) & (pal1y < info[f]['ymax']))
+    pal1x = pal1x[w]
+    pal1y = pal1y[w]
+    pal1RA = pal1['X_WORLD'][w]
+    pal1Dec = pal1['Y_WORLD'][w]
+    ID = pal1['NUMBER'][w]
     if len(Palcats) == 2:
         # 2 filters used
         # g in dual image mode
-        AUTO_g = pal1['MAG_AUTO']
-        eAUTO_g = pal1['MAGERR_AUTO']
+        AUTO_g = pal1['MAG_AUTO'][w]
+        eAUTO_g = pal1['MAGERR_AUTO'][w]
         # i 
         pal2 = read_cat(Palcats[1])
-        AUTO_i = pal2['MAG_AUTO']
-        eAUTO_i = pal2['MAGERR_AUTO']
+        AUTO_i = pal2['MAG_AUTO'][w]
+        eAUTO_i = pal2['MAGERR_AUTO'][w]
 
         # calibrate photometry
         color = AUTO_g - AUTO_i
@@ -209,8 +247,8 @@ def make_cat(info, WISPfilters, threshold, wispfield):
         
     elif len(Palcats) == 1:
         if filts[0] == 'g':
-            AUTO_g = pal1['MAG_AUTO']
-            eAUTO_g = pal1['MAGERR_AUTO']
+            AUTO_g = pal1['MAG_AUTO'][w]
+            eAUTO_g = pal1['MAGERR_AUTO'][w]
             mag_i = np.array([99.99]*pal1RA.shape[0])
             emag_i = np.array([-9.99]*pal1RA.shape[0])
             # calibrate photometry
@@ -220,14 +258,14 @@ def make_cat(info, WISPfilters, threshold, wispfield):
         if filts[0] == 'i':
             mag_g = np.array([99.99]*pal1RA.shape[0])
             emag_g = np.array([-9.99]*pal1RA.shape[0])
-            AUTO_i = pal1['MAG_AUTO']
-            eAUTO_i = pal1['MAGERR_AUTO']
+            AUTO_i = pal1['MAG_AUTO'][w]
+            eAUTO_i = pal1['MAGERR_AUTO'][w]
             # calibrate photometry
             mag_i,emag_i = calibrate(AUTO_i, eAUTO_i, info['i']['zp'],
                 info['i']['maglim'])
 
     # add all sources to region file
-    make_reg(pal1RA, pal1Dec, reg, 2, 'blue', nsource=pal1['NUMBER'])
+    make_reg(pal1RA, pal1Dec, reg, 2, 'blue', nsource=ID)
     reg.close()
 
     # construct Palomar catalog
@@ -265,32 +303,45 @@ def make_cat(info, WISPfilters, threshold, wispfield):
         else:
             mag_110 = np.array([99.99]*wispRA.shape[0])
             emag_110 = np.array([-9.99]*wispRA.shape[0])
-        if os.path.join('fin_F140.cat') in WISPfilters:
+        if os.path.join(wispfield,'fin_F140.cat') in WISPfilters:
             t140 = read_cat(os.path.join(wispfield,'fin_F140.cat'))
-            mag_140 = t140['MAG_F1153W']
+            mag_140 = t140['MAG_F1392W']
             emag_140 = t140['MAGERR_AUTO']
         else:
             mag_140 = np.array([99.99]*wispRA.shape[0])
             emag_140 = np.array([-9.99]*wispRA.shape[0])
-        if os.path.join('fin_F160.cat') in WISPfilters:
-            t160 = read_cat(os.path.join('fin_F160.cat'))
-            mag_160 = t160['MAG_F1153W']
+        if os.path.join(wispfield,'fin_F160.cat') in WISPfilters:
+            t160 = read_cat(os.path.join(wispfield,'fin_F160.cat'))
+            mag_160 = t160['MAG_F1573W']
             emag_160 = t160['MAGERR_AUTO']
         else:
             mag_160 = np.array([99.99]*wispRA.shape[0])
             emag_160 = np.array([-9.99]*wispRA.shape[0])
 
         # match Palomar to WISP
+        '''
         idx,separc = match_cats(wispRA, wispDec, pal1RA, pal1Dec)
         match = (separc.value*3600. <= threshold)
         nomatch = (separc.value*3600. > threshold)
         print '\n%i WISP objs matched to Palomar sources' % idx[match].shape[0]
         print '%i WISP objs NOT matched to Palomar sources\n' % \
             idx[nomatch].shape[0]
+        '''
+        idx,separc = match_cats(pal1RA, pal1Dec, wispRA, wispDec)
+        match = (separc.value*3600. <= threshold)
+        nomatch = (separc.value*3600. > threshold)
+        print '\n%i Palomar objs matched to WISP sources' % idx[match].shape[0]
 
         # add successfully matched sources to region file with width=4
-        reg = open(os.path.join(wispfield,'Palomar-final.reg'), 'a')
-        make_reg(wispRA[match], wispDec[match], reg, 1.5, 'green')
+        reg = open(os.path.join(wispfield,'WISP.reg'), 'w')
+        reg.write('global color=green dashlist=8 3 width=1 font="helvetica 10 '
+                'normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 '
+                'move=1 delete=1 include=1 source=1 \n')
+        reg.write('fk5 \n')
+        make_reg(wispRA[idx[match]], wispDec[idx[match]], reg, 1.5, 
+                 'green', width=4)
+        make_reg(wispRA, wispDec, reg, 1.5, 'green')
+        reg.close()
 
         # create arrays for info from Palomar catalog
         palRA = np.array([99.99]*wispRA.shape[0])
@@ -303,7 +354,7 @@ def make_cat(info, WISPfilters, threshold, wispfield):
         palarrays = [pal1RA, pal1Dec, mag_g, emag_g, mag_i, emag_i]   
 
         for warr,parr in zip(wisparrays,palarrays):
-            warr[match] = parr[idx[match]]
+            warr[idx[match]] = parr[match]
 
         wispCat = Table([ID, wispRA, wispDec, palRA, palDec, pal_g, epal_g,
                          pal_i, epal_i, mag_110, emag_110, mag_140, emag_140,
@@ -325,11 +376,11 @@ def make_cat(info, WISPfilters, threshold, wispfield):
         WISPimage = info['WISPim']
         WISPim,WISPhdr = pyfits.getdata(WISPimage, header=True)
         cRA = WISPhdr['CRVAL1'] 
-        cDec = WISPhdr['CRVAL2'] 
+        cDec = WISPhdr['CRVAL2']
         xsize,ysize = 3.,3.
         for filt in filts:
             Palim = os.path.join(wispfield, '%s_%s.fits'%(wispfield,filt))
-            outname = os.path.splitext(Palim)[0] + '_WISP.fits'
+            outname = os.path.splitext(Palim)[0] + '_WISPFOV.fits'
             trim_image(Palim, cRA, cDec, xsize, ysize, outname)
 
 
@@ -339,8 +390,12 @@ def main():
     parser.add_argument('wispfield', type=str, nargs=1,
         help='WISP field for which to construct a catalog. ' +\
              'Must match name of directory containing all relevant files')
+    parser.add_argument('--threshold', type=float, default=0.75,
+        help='Radius in arcsec to be used for matching Palomar and WISP '+\
+             'catalogs. Default is 0.75"')
     args = parser.parse_args()
     wispfield = args.wispfield[0].strip('/')
+    threshold = args.threshold
 
     # setup for creation of catalog
     # info contains colorterms, zp's, maglimits, the background sigma
@@ -350,7 +405,6 @@ def main():
     # construct catalog
     WISPfilters = glob(os.path.join(wispfield,'fin_*.cat'))
     WISPfilters.sort()
-    threshold = 0.5  # arcsec for matching matching
     make_cat(info, WISPfilters, threshold, wispfield)
 
 
